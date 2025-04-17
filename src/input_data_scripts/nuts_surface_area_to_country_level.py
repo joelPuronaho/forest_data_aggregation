5f9597204b068146629f100d3121ecea52e138f5
joelPuronaho <joel.puronaho@luke.fi>
2025-04-17 14:32:49 +0300

    Typos fixed

--- src/input_data_scripts/nuts_surface_area_to_country_level.py
+++ src/input_data_scripts/nuts_surface_area_to_country_level.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from io import StringIO
 
-# Your input data
+# Load data
 data = "../../input_data/filtered_nuts2_surface_areas_landuse_total.csv"
 
-# Read data into a DataFrame
+# Read data into a df
 df = pd.read_csv(data, sep=';')
 print(df)
 
-# Extract country code (first two letters of NUTS_ID)
+# Extract first two letters of NUTS_ID
 df['country_code'] = df['NUTS_ID'].str[:2]
 
 # Group by country and sum surface area
@@ -16,15 +16,14 @@ country_df = df.groupby('country_code', as_index=False).agg({
     'official_surface_area_2021': 'sum'
 })
 
-# Add remaining constant columns
+# Add remaining columns
 country_df['freq'] = 'A'
 country_df['landuse'] = 'TOTAL'
 country_df['unit'] = 'KM2'
 
 # Rename column to match format
+# Fix?
 country_df.rename(columns={'country_code': 'NUTS_ID'}, inplace=True)
 
-# Save to CSV
+# Save as csv
 country_df.to_csv('../../input_data/country_level_filtered_nuts2_surface_areas_landuse_total.csv', sep=';', index=False)
-
-print(country_df)
